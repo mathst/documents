0949dedc77cf4818aa94b536ac8d9b399a680845
mathst <mathkol@sempreceub.com>
2022-03-24 00:46:33 -0300

    a

--- api/src/app.py
+++ api/src/app.py
@@ -6,12 +6,17 @@ import back
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/a', methods=['GET'])
 def funct():
     """ Function which is triggered in flask app """
     meses = 12
     result = back.doing("ipca.xlsx",meses)
     return str(result)
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
