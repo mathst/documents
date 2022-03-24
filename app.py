243e82ff37a52fdf97cee26537034ede5665b278
mathst <mathkol@sempreceub.com>
2022-03-24 10:56:39 -0300

    aaa

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-import src.back as back 
+import src.back as bk
+import os
 
 # app.py
 from flask import Flask, request, jsonify
@@ -9,7 +10,7 @@ app = Flask(__name__)
 def funct():
     """ Function which is triggered in flask app """
     meses = 12
-    result = back.doing("ipca.xlsx",meses)
+    result = bk.doing("ipca.xlsx",meses)
     return str(result)
 
 # A welcome message to test our server
@@ -18,8 +19,8 @@ def index():
     return "<h1>the result is in '/a'</h1>"
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port = port)
 
 
 
