c784fffaa710aca25e2762875f95f3a1fb4f702e
mathst <mathkol@sempreceub.com>
2022-03-24 00:42:10 -0300

    b

--- api/src/app.py
+++ api/src/app.py
@@ -11,17 +11,11 @@ def funct():
     """ Function which is triggered in flask app """
     meses = 12
     result = back.doing("ipca.xlsx",meses)
-    return str(result) | logging.error()
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    """ Function which is triggered in flask app """
-    meses = 12
-    result = back.doing("ipca.xlsx",meses)
-    return f'<h1>{str(result) | logging.error()}</h1>'
+    return str(result)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
+
+
 
